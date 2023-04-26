7a52bf20aeb154e0682ae35be43aff6713de37f9
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-04-27 01:24:56 +0300

    Login controller added - updated README - login route added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-![NodeJS](https://github.com/k-d-ivanov/observiote-api/actions/workflows/nodejs.yml/badge.svg)  
+![NodeJS](https://github.com/k-d-ivanov/observiote-api/actions/workflows/nodejs.yml/badge.svg)
 
 # observiote-api
 
 Simple API based on Node.js, Express and MongoDB for the observiote project
-The Observiote project is located here: [LINK](https://github.com/k-d-ivanov/observiote)
+The Observiote project is located here: [LINK](https://github.com/kirildi/observiote)
--- server.ts
+++ server.ts
@@ -1,11 +1,20 @@
+/*
+    Observiote-API Application
+    Copyright (C) 2023 Kiril Ivanov
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ */
 import app from "./src/App.js";
-import { Response, Request } from "express";
 import mongoose from "mongoose";
 import { HOST_URL, HOST_PORT, API_BASE } from "./src/config.js";
-
-app.get(API_BASE, (req: Request, res: Response) => {
-  return res.json({ message: "Hello World" });
-});
 
 mongoConnect()
   .then(() => {
