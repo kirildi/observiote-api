3ae846b2d5c9368e41927a13cede831bebf2b993
Kiril Ivanov <kiril.ivanov91@gmail.com>
2023-04-27 21:31:07 +0300

    Added device, sensor, and login controllers - just added the controllers, the logic will be added in other commits - added the route objects which will handle the different routes

---
+++ src/controllers/DeviceController.ts
@@ -0,0 +1,24 @@
+/*
+    Observiote-API Application
+    Copyright (C) 2023 Kiril Ivanov
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ */
+
+import DeviceModel from "../models/DeviceModel.js";
+
+class DeviceController {
+  constructor() {}
+
+  #deviceModel = new DeviceModel();
+}
+
+export default DeviceController;
---
+++ src/controllers/UserController.ts
@@ -0,0 +1,24 @@
+/*
+    Observiote-API Application
+    Copyright (C) 2023 Kiril Ivanov
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ */
+
+import UserModel from "../models/UserModel.js";
+
+class UserController {
+  constructor() {}
+
+  #userModel = new UserModel();
+}
+
+export default UserController;
